23be33a22e10bb2bff39bb92e7a99081f4a0e35c
ayaz07 <118646687+Ayaz55555@users.noreply.github.com>
2026-01-12 18:06:10 +0530

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
   dependencies: {
-    'react-native-aes-crypto-forked': {
+    'react-native-aes-crypto-forke': {
       platforms: {
         ios: null, // disable Android platform, other platforms will still autolink if provided
       },
